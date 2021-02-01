bb3bc6b8a1c610497d17bbb7294a7783c18410bd
Jiuming Qin <jiuming.qin19@imperial.ac.uk>
2021-02-01 20:26:42 +0800

    Add a new class Pattern:
    Can perform elementary operations on the game board patterns.

--- life/life.py
+++ life/life.py
@@ -54,6 +54,7 @@ class Game:
         game.board = np.zeros((Size, Size))
 
     def play(self):
+        """Start a new game of life and play."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
@@ -62,22 +63,57 @@ class Game:
             pyplot.pause(0.0000005)
 
     def move(self):
+        """Move the object for one step on the board. """
         STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
         NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
                 self.board[i, j] = 1 if (
-                    NeighbourCount[i, j] == 3
-                    or (NeighbourCount[i, j] == 2
-                        and self.board[i, j])
-                )  # noqa E999
-                else 0
+                    NeighbourCount[i, j] == 3 or (
+                        NeighbourCount[i, j] == 2 and self.board[i, j]
+                    )
+                ) else 0
 
     def __setitem__(self, key, value):
+        """Set the value of a particular position on the board. """
         self.board[key] = value
 
     def show(self):
+        """Visualise the gameboard. """
         pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
+
+class Pattern:
+    def __init__(self, pattern):
+        self.grid = pattern
+        self.shape = self.grid.shape
+        self.nrows, self.ncols = self.shape
+
+    def flip_vertical(self):
+        """Returns a new Pattern whose rows are in reversed order. """
+        return Pattern(self.grid[::-1, :])
+
+    def flip_horizontal(self):
+        """Returns a new Pattern whose columns are in reversed order. """
+        return Pattern(self.grid[:, ::-1])
+
+    def flip_diag(self):
+        """Returns a new Pattern that is the transpose of the original. """
+        new_pattern = np.zeros((self.ncols, self.nrows))
+        for i in range(self.ncols):
+            for j in range(self.nrows):
+                new_pattern[i, j] = self.grid[j, i]
+        return Pattern(new_pattern)
+
+    def rotate(self, n):
+        """
+        Return a new Pattern which is the original pattern
+        rotated through n right angles anticlockwise.
+        """
+        arr = self
+        for _ in range(n):
+            arr = arr.flip_diag().flip_vertical()
+        return arr
